9e4e066c9de0188610fbaa9149cd371f0e0fd815
Bruno Gonçalves <brunogcp28@gmail.com>
2023-09-21 08:32:21 -0400

    Variaveis em JS

---
+++ index.js
@@ -0,0 +1,18 @@
+// var pessoa = "Bruno"
+
+// const emailPessoa = "bruno@gmail.com"
+
+
+// let telefonePessoa = "996658417"
+
+// telefonePessoa = "9966584758"
+
+
+// let retornoDoUsuario = confirm("Deseja continuar")
+
+// console.log(retornoDoUsuario)
+
+let idade = prompt("Digite sua idade:")
+
+console.log(idade)
+
